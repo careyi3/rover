cde7ff5931cd1b8d43f197ab699a72a5b2ab7437
careyi3 <ian@iancarey.ie>
2022-10-08 15:24:23 +0100

    Added repl interface

--- pi/serial_test.rb
+++ pi/serial_test.rb
@@ -1,9 +1,11 @@
 require 'pry'
 require 'serialport'
+require 'readline'
 
 sem = Mutex.new
 
-ser = SerialPort.new("/dev/ttyACM0", 9600, 8, 1, SerialPort::NONE)
+# ser = SerialPort.new("/dev/ttyACM0", 9600, 8, 1, SerialPort::NONE)
+ser = SerialPort.new("/dev/tty.usbmodem1101", 9600, 8, 1, SerialPort::NONE)
 
 data = []
 
@@ -21,13 +23,15 @@ reader = Thread.new {
 def handleData(sem, data)
   sem.synchronize {
     if data.length > 0
-      puts data.shift
+      # puts data.shift
     end
   }
 end
 
 def runCommands(ser)
-  ser.write("F123\n")
+  while buf = Readline.readline("> ", true)
+    ser.write("#{buf}\n")
+  end
 end
 
 while true
